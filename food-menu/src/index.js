1adacc5628caa5c8335cf4161ac15a8f6262b1df
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-06-23 21:06:20 +0700

    create components

--- food-menu/src/index.js
+++ food-menu/src/index.js
@@ -2,12 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello, React</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Warung Mang Ucok</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Menu Kita</h2>
+      <Food />
+      <Food />
+      <Food />
+      <Food />
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>{new Date().getFullYear()} Warung Mang Ucok</footer>;
+}
+
+function Food() {
+  return (
+    <div>
+      <img
+        src="food/soto-betawi.jpg"
+        alt="Soto Betawi"
+        width={100}
+        height={70}
+      />
+      <h2>Soto Betawi</h2>
+      <p>Soto Betawi dari Jakarta</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App></App>
+    <App />
   </React.StrictMode>
 );
