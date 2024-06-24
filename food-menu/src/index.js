78081e9a41a406365263ecbd0eecbb308ea9d5ea
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-06-24 11:08:18 +0700

    display ui in jsx with ternary if

--- food-menu/src/index.js
+++ food-menu/src/index.js
@@ -25,95 +25,34 @@ function Header() {
 }
 
 function Menu() {
-  // const foods = [];
-  const foods = data;
+  const foods = [];
+  // const foods = data;
   const numFoods = foods.length;
   return (
     <main className="menu">
       <h2>Menu Kita</h2>
-      {numFoods > 0 && (
+      {numFoods > 0 ? (
         <ul className="foods">
           {data.map((food) => (
             <Food foodObj={food} key={food.nama} />
           ))}
         </ul>
+      ) : (
+        <p>Lagi mudik, Senin depan baru buka.</p>
       )}
-      {/* <Food
-        nama="Nasi Goreng"
-        deskripsi="Nasi yang digoreng dengan bumbu rempah khas Indonesia"
-        harga={25000}
-        foto="food/nasi-goreng.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Sate Ayam"
-        deskripsi="Potongan daging yam yang ditusuk dan dibakar, disajikan dengan bumbu kacang"
-        harga={15000}
-        foto="food/sate-ayam.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Gado-Gado"
-        deskripsi="Sayuran segar dengan bumbu kacang khas Indonesia"
-        harga={20000}
-        foto="food/gado-gado.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Rendang"
-        deskripsi="Daging yang dimasak dengan santan dan rempah-rempah khas Padang"
-        harga={30000}
-        foto="food/rendang.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Soto Ayam"
-        deskripsi="Sup ayam dengan kuah kuning dan berbagai rempah"
-        harga={18000}
-        foto="food/soto-ayam.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Martabak"
-        deskripsi="Makanan dadakan yang terbuat dari adonan tepung dengan berbagai isian"
-        harga={20000}
-        foto="food/martabak.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Bakso"
-        deskripsi="Bola daging yang direbus disajikan dengan mie dan kuah kaldu"
-        harga={12000}
-        foto="food/bakso.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Soto Betawi"
-        deskripsi="Sup daging dengan kuah santan kental khas Betawi"
-        harga={22000}
-        foto="food/soto-betawi.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      />
-      <Food
-        nama="Ayam Penyet"
-        deskripsi="Ayam yang digeprek dengan bumbu rempah dan disajikan dengan sambal"
-        harga={18000}
-        foto="food/ayam-penyet.jpg"
-        stok={Math.random() >= 0.5 ? true : false}
-      /> */}
     </main>
   );
 }
 
 function Footer() {
   const hour = new Date().getHours();
-  const jamBuka = 7;
+  const jamBuka = 17;
   const jamTutup = 22;
   const isOpen = hour >= jamBuka && hour <= jamTutup;
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>
             {new Date().getFullYear()} Warteg Mang Ucok | jam buka {jamBuka} -
@@ -121,6 +60,10 @@ function Footer() {
           </p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          maap masih tutup, bukanya jam {jamBuka} - {jamTutup}
+        </p>
       )}
     </footer>
   );
