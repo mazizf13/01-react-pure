c4bcc3a233b803ba47299f16f0684c70d8a126da
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-06-28 13:46:59 +0700

    react developer tools

--- steps-form/src/App.js
+++ steps-form/src/App.js
@@ -1,52 +1,49 @@
 import { useState } from "react";
 
+const stepItems = ["Dream", "Believe", "Archieve"];
+
 function App() {
-  // const arr = useState({ name: "az" });
-  // console.log(arr);
-  // let step = 1;
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({ name: "az" });
+  const [isOpen, setIsOpen] = useState(true);
 
   function handleNext() {
-    // step += 1;
     if (step < 3) setStep(step + 1);
-    console.log(step);
-    // bad practice
-    test.name = "fr";
-    // console.log(step);
-
-    setTest({ name: "fir" });
   }
 
   function handlePrev() {
-    // step += 1;
     if (step > 1) setStep(step - 1);
-    // console.log(step);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+        <p className="message">
+          Step {step}: {stepItems[step - 1]}
+        </p>
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "#526D82", color: "#fff" }}
+            onClick={handlePrev}
+          >
+            Prev
+          </button>
+          <button
+            style={{ backgroundColor: "#526D82", color: "#fff" }}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
       </div>
-      <p className="message">Step: Dream {test.name}</p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#526D82", color: "#fff" }}
-          onClick={handlePrev}
-        >
-          Prev
-        </button>
-        <button
-          style={{ backgroundColor: "#526D82", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 
