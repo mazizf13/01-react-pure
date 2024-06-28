75172a7c15e2d00d29c5e5b26878aa47342e1466
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-06-28 13:11:47 +0700

    fill in the usestate and dont change the value directly

--- steps-form/src/App.js
+++ steps-form/src/App.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 
 function App() {
+  // const arr = useState({ name: "az" });
+  // console.log(arr);
   // let step = 1;
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "az" });
+
   function handleNext() {
     // step += 1;
     if (step < 3) setStep(step + 1);
+    console.log(step);
+    // bad practice
+    test.name = "fr";
     // console.log(step);
+
+    setTest({ name: "fir" });
   }
 
   function handlePrev() {
@@ -22,7 +31,7 @@ function App() {
         <div className={step >= 2 ? "active" : ""}>2</div>
         <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">Step: Dream</p>
+      <p className="message">Step: Dream {test.name}</p>
       <div className="buttons">
         <button
           style={{ backgroundColor: "#526D82", color: "#fff" }}
