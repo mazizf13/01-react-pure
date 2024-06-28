0db5dc45289370b2ddec99f6be9867518ee10348
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-06-28 10:23:54 +0700

    using event handler in components

---
+++ steps-form/src/App.js
@@ -0,0 +1,32 @@
+function App() {
+  function handleNext() {
+    alert("Next Button Clicked");
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div className="">2</div>
+        <div className="">3</div>
+      </div>
+      <p className="message">Step: Dream</p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#526D82", color: "#fff" }}
+          onMouseEnter={() => alert("Prev Hover")}
+        >
+          Prev
+        </button>
+        <button
+          style={{ backgroundColor: "#526D82", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
