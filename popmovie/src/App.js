6c2a2906545d66526e77919ed0ea7fd3669b2575
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-07-01 18:34:02 +0700

    feat: overcoming prop drilling with component composition

--- popmovie/src/App.js
+++ popmovie/src/App.js
@@ -80,14 +80,8 @@ function NumResults({ movies }) {
   );
 }
 
-function NavBar({ movies }) {
-  return (
-    <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResults movies={movies} />
-    </nav>
-  );
+function NavBar({ children }) {
+  return <nav className="nav-bar">{children}</nav>;
 }
 
 function MovieList({ movies }) {
@@ -189,13 +183,8 @@ function WatchedList() {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <MovieList movies={movies} />
-      <WatchedList />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
 export default function App() {
@@ -203,8 +192,15 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <MovieList movies={movies} />
+        <WatchedList />
+      </Main>
     </>
   );
 }
