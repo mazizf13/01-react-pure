97af1e83c1285087c39d2022923d517991289882
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-07-01 08:36:03 +0700

    feat: create an event handler to cretae friend data object

--- split-bill/src/components/FormAddFriend.js
+++ split-bill/src/components/FormAddFriend.js
@@ -4,8 +4,25 @@ export default function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    onAddFriend(newFriend);
+    setName("");
+    setImage("");
+  }
+
   return (
-    <form action="" className="form-add-friend">
+    <form action="" className="form-add-friend" onSubmit={handleSubmit}>
       <label htmlFor="">👋 Nama</label>
       <input
         type="text"
