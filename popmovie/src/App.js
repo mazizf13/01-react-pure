d2e407ce932dc16ef220c2ab28875658552d3beb
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-07-07 14:45:22 +0700

    async function in react

--- popmovie/src/App.js
+++ popmovie/src/App.js
@@ -196,9 +196,16 @@ export default function App() {
   const [watched, setWatched] = useState(tempWatchedData);
 
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?&apikey=${API_KEY}&s=oppenheimer`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovie() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?&apikey=${API_KEY}&s=oppenheimer`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      console.log(data.Search);
+    }
+
+    fetchMovie();
   }, []);
 
   return (
--- popmovie/src/index.js
+++ popmovie/src/index.js
@@ -6,10 +6,10 @@ import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-    {/* <StarRating max={5} color="#fcc419" size={40} />
-    <StarRating max={10} color="pink" size={50} />
-    <StarRating max={1} color="red" /> */}
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // {/* <StarRating max={5} color="#fcc419" size={40} />
+  // <StarRating max={10} color="pink" size={50} />
+  // <StarRating max={1} color="red" /> */}
+  // </React.StrictMode>
 );
